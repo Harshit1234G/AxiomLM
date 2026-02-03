ebeec11f97b204d23a75cc2cd6b72c3a49f59b29
Harshit <harshitkumawat849@gmail.com>
2026-02-03 20:22:43 +0530

    Implemented sinusoidal positional encoding

--- kaggle/input/axiom-utils/llm_components.py
+++ kaggle/input/axiom-utils/llm_components.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 from sentencepiece import SentencePieceProcessor
 
 
@@ -8,6 +9,7 @@ from sentencepiece import SentencePieceProcessor
 SEQUENCE_LEN = 256
 BATCH_SIZE = 8
 SHUFFLE_BUFFER = 10_000
+N_EMBEDS = 512
 
 
 # ----------------------------
@@ -59,12 +61,10 @@ class LMDatasetLoader:
             self.tf_sp_tokenize,
             num_parallel_calls= AUTOTUNE
         )
-
         # Flatten into one continuous token stream
         ds = ds.flat_map(
             lambda x: tf.data.Dataset.from_tensor_slices(x)
         )
-
         # creating sequences of length (seq_len + 1)
         ds = ds.batch(self.seq_len + 1, drop_remainder= True)
 
@@ -80,3 +80,36 @@ class LMDatasetLoader:
         ds = ds.batch(self.batch_size)
         ds = ds.prefetch(AUTOTUNE)
         return ds
+    
+
+# --------------------------------
+# Sinusoidal Positional Encoding
+# --------------------------------
+class PositionalEncoding(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        *,
+        max_seq_len: int = SEQUENCE_LEN,
+        embed_size: int = N_EMBEDS,
+        dtype: tf.dtypes.DType = tf.float32,
+        **kwargs
+    ) -> None:
+        super().__init__(dtype= dtype, **kwargs)
+        assert embed_size % 2 == 0, 'embed_size must be even'
+
+        # p -> each column is a position index
+        # i -> each row corresponds to even embedding dimensions
+        p, i = np.meshgrid(
+            np.arange(max_seq_len),
+            2 * np.arange(embed_size // 2)
+        )
+        # initializing PE matrix
+        pos_emb = np.empty((1, max_seq_len, embed_size))   # (Batch, Positions, Embedding dimensions)
+        pos_emb[0, :, ::2] = np.sin(p / 10_000 ** (i / embed_size)).T    # sine on even dimensions
+        pos_emb[0, :, 1::2] = np.cos(p / 10_000 ** (i / embed_size)).T   # cosine on odd dimensions
+        self.pos_encodings = tf.constant(pos_emb.astype(self.dtype))
+        self.supports_masking = True    # propagates the input's automatic mask to next layer
+
+    def call(self, inputs):
+        batch_max_length = tf.shape(inputs)[1]
+        return inputs + self.pos_encodings[:, :batch_max_length]
